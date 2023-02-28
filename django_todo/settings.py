3d2a5710459c3c8d12b5ae589ce593d5c5dcb861
AlinaPanasenko <maikshev@gmail.com>
2023-02-28 21:12:18 +0000

    Add superuser and model

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-
-SECRET_KEY = config("SECRET_KEY")
+import os
+if os.path.isfile('env.py'):
+    import env
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
